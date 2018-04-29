af9878b35a0b12a29dcfb63a81b2603ca4b50126
willstrauss <w.strauss@wustl.edu>
2018-04-29 13:48:42 -0500

    remove commented out code

--- data_augmentation/data_aug.py
+++ data_augmentation/data_aug.py
@@ -1,16 +1,6 @@
 from imgaug import augmenters as iaa
 
 def get_augmentor():
-    # iaa.seed(1)
-    # augmentation = iaa.SomeOf((0, 2), [
-    #     iaa.Fliplr(0.5),
-    #     iaa.Flipud(0.5),
-    #     iaa.OneOf([iaa.Affine(rotate=90),
-    #                 iaa.Affine(rotate=180),
-    #                 iaa.Affine(rotate=270)]),
-    #     iaa.Multiply((0.8, 1.5)),
-    #     iaa.GaussianBlur(sigma=(0.0, 5.0))
-    # ])
     augmentation = iaa.SomeOf((0, 2), [
         iaa.Fliplr(0.5),  # horizontal flip
         iaa.Flipud(0.5),  # vertical flip
