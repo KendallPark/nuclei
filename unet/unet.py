ff1a6fe9e18305e9fb2718a9530f97cfb66c2822
shynehua <shynehua@gmail.com>
2018-04-29 23:22:56 -0500

    new unet.py

---
+++ unet/unet.py
@@ -0,0 +1,159 @@
+import os
+import sys
+import random
+import warnings
+import skimage.io
+import numpy as np
+import pandas as pd
+import cv2
+import matplotlib.pyplot as plt
+import skimage.segmentation
+from sklearn.model_selection import train_test_split
+from keras.models import Model, load_model
+from keras.layers import *
+from keras.optimizers import Adam
+from keras.regularizers import l2
+from keras.preprocessing.image import ImageDataGenerator
+import keras.backend as K
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint, EarlyStopping
+import tensorflow as tf
+from tqdm import tqdm
+from itertools import chain
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+
+
+# params
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNELS = 3
+TRAIN_PATH = 'data/stage1_train/'
+TEST_PATH = 'data/stage1_test/'
+
+warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
+seed = 42
+random.seed = seed
+np.random.seed = seed
+
+num_of_epoch = 100
+filename = 'model-no-augmentation.h5'
+
+def load_resize_data():
+    # Get train and test IDs
+    train_ids = next(os.walk(TRAIN_PATH))[1]
+    test_ids = next(os.walk(TEST_PATH))[1]
+
+    # Get and resize train images and masks
+    X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+    print('Getting and resizing train images and masks ... ')
+    sys.stdout.flush()
+    for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+        path = TRAIN_PATH + id_
+        img = imread(path + '/images/' + id_ + '.png')[:, :, :IMG_CHANNELS]
+        img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        X_train[n] = img
+        mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+        for mask_file in next(os.walk(path + '/masks/'))[2]:
+            mask_ = imread(path + '/masks/' + mask_file)
+            mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant',
+                                          preserve_range=True), axis=-1)
+            mask = np.maximum(mask, mask_)
+        Y_train[n] = mask
+
+    # Get and resize test images
+    X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    sizes_test = []
+    print('Getting and resizing test images ... ')
+    sys.stdout.flush()
+    for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
+        path = TEST_PATH + id_
+        img = imread(path + '/images/' + id_ + '.png')[:, :, :IMG_CHANNELS]
+        sizes_test.append([img.shape[0], img.shape[1]])
+        img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        X_test[n] = img
+
+    print('Done!')
+    return X_train, Y_train
+
+def get_validation(X_train, Y_train):
+
+
+def mean_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
+
+
+def build_unet():
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    s = Lambda(lambda x: x / 255)(inputs)
+
+    c1 = Conv2D(64, (3, 3), activation='relu', padding='same')(s)
+    c1 = Conv2D(64, (3, 3), activation='relu', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+
+    c2 = Conv2D(128, (3, 3), activation='relu', padding='same')(p1)
+    c2 = Conv2D(128, (3, 3), activation='relu', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+
+    c3 = Conv2D(256, (3, 3), activation='relu', padding='same')(p2)
+    c3 = Conv2D(256, (3, 3), activation='relu', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+
+    c4 = Conv2D(512, (3, 3), activation='relu', padding='same')(p3)
+    c4 = Conv2D(512, (3, 3), activation='relu', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+
+    c5 = Conv2D(1024, (3, 3), activation='relu', padding='same')(p4)
+    c5 = Conv2D(1024, (3, 3), activation='relu', padding='same')(c5)
+
+    u6 = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(512, (3, 3), activation='relu', padding='same')(u6)
+    c6 = Conv2D(512, (3, 3), activation='relu', padding='same')(c6)
+
+    u7 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(256, (3, 3), activation='relu', padding='same')(u7)
+    c7 = Conv2D(256, (3, 3), activation='relu', padding='same')(c7)
+
+    u8 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(128, (3, 3), activation='relu', padding='same')(u8)
+    c8 = Conv2D(128, (3, 3), activation='relu', padding='same')(c8)
+
+    u9 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(64, (3, 3), activation='relu', padding='same')(u9)
+    c9 = Conv2D(64, (3, 3), activation='relu', padding='same')(c9)
+    c9 = Conv2D(2, 3, activation='relu', padding='same')(c9)
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[mean_iou])
+    model.summary()
+
+    return model
+
+def train(X_train, Y_train, X_val, Y_val, model, filename):
+    earlystopper = EarlyStopping(patience=5, verbose=1)
+    checkpointer = ModelCheckpoint(filename, verbose=1, save_best_only=True)
+    results = model.fit(X_train, Y_train, validation_data=(X_val, Y_val), batch_size=8, epochs=150,
+                        callbacks=[checkpointer])
+
+def main():
+    [X_train, Y_train] = load_resize_data()
+    model = build_unet()
+    [X_train, Y_train, X_val, Y_val] = get_validation(X_train, Y_train)
+    train(X_train, Y_train, X_val, Y_val, model, filename)
+
+if __name__ == '__main__':
+    main()
