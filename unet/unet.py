da84d1f676606dbd9c7b10c9110edba61085b9c9
shynehua <shynehua@gmail.com>
2018-04-29 23:38:26 -0500

    update unet.py

--- unet/unet.py
+++ unet/unet.py
@@ -22,7 +22,7 @@ from itertools import chain
 from skimage.io import imread, imshow, imread_collection, concatenate_images
 from skimage.transform import resize
 from skimage.morphology import label
-
+from data.extractor_id import extraction
 
 # params
 IMG_WIDTH = 128
@@ -39,10 +39,8 @@ np.random.seed = seed
 num_of_epoch = 100
 filename = 'model-no-augmentation.h5'
 
-def load_resize_data():
+def load_resize_data(train_ids, validation_ids):
     # Get train and test IDs
-    train_ids = next(os.walk(TRAIN_PATH))[1]
-    test_ids = next(os.walk(TEST_PATH))[1]
 
     # Get and resize train images and masks
     X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
@@ -62,22 +60,37 @@ def load_resize_data():
             mask = np.maximum(mask, mask_)
         Y_train[n] = mask
 
-    # Get and resize test images
-    X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
-    sizes_test = []
-    print('Getting and resizing test images ... ')
+    X_val = np.zeros((len(validation_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    Y_val = np.zeros((len(validation_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+    print('Getting and resizing validation images and masks ... ')
     sys.stdout.flush()
-    for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
-        path = TEST_PATH + id_
+    for n, id_ in tqdm(enumerate(validation_ids), total=len(validation_ids)):
+        path = TRAIN_PATH + id_
         img = imread(path + '/images/' + id_ + '.png')[:, :, :IMG_CHANNELS]
-        sizes_test.append([img.shape[0], img.shape[1]])
         img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
-        X_test[n] = img
+        X_val[n] = img
+        mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+        for mask_file in next(os.walk(path + '/masks/'))[2]:
+            mask_ = imread(path + '/masks/' + mask_file)
+            mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant',
+                                          preserve_range=True), axis=-1)
+            mask = np.maximum(mask, mask_)
+        Y_val[n] = mask
 
     print('Done!')
-    return X_train, Y_train
+    return X_train, Y_train, X_val, Y_val
 
-def get_validation(X_train, Y_train):
+
+def get_data():
+    validation_ids = extraction(0.1, "data/stage1_train_classses.csv", "data/stage1_train")
+    all_ids = next(os.walk(TRAIN_PATH))[1]
+    train_ids = [value for value in all_ids if value not in validation_ids]
+
+    print("## Train IDs")
+    print(train_ids)
+    print("## Validation IDs")
+    print(validation_ids)
+    return load_resize_data(train_ids, validation_ids)
 
 
 def mean_iou(y_true, y_pred):
@@ -143,17 +156,19 @@ def build_unet():
 
     return model
 
+
 def train(X_train, Y_train, X_val, Y_val, model, filename):
     earlystopper = EarlyStopping(patience=5, verbose=1)
     checkpointer = ModelCheckpoint(filename, verbose=1, save_best_only=True)
     results = model.fit(X_train, Y_train, validation_data=(X_val, Y_val), batch_size=8, epochs=150,
                         callbacks=[checkpointer])
 
+
 def main():
-    [X_train, Y_train] = load_resize_data()
     model = build_unet()
-    [X_train, Y_train, X_val, Y_val] = get_validation(X_train, Y_train)
+    [X_train, Y_train, X_val, Y_val] = get_data()
     train(X_train, Y_train, X_val, Y_val, model, filename)
+
 
 if __name__ == '__main__':
     main()
