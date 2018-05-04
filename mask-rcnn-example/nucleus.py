46b32b46956f20d24d9e22853968bd170dc94dc0
Kendall Park <darkidder@gmail.com>
2018-05-03 23:25:24 -0500

    feat: augmentation 2

--- mask-rcnn-example/nucleus.py
+++ mask-rcnn-example/nucleus.py
@@ -299,14 +299,14 @@ def train(model):
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=20,
-                augmentation=augmentation_1,
+                augmentation=augmentation_2,
                 layers='heads')
 
     print("Train all layers")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=80,
-                augmentation=augmentation_1,
+                augmentation=augmentation_2,
                 layers='all')
 
 
