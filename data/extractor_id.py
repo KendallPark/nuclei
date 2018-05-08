4dfdeb39de3ae6fd2da58368ac9537dade01b85c
Kendall Park <darkidder@gmail.com>
2018-05-08 05:34:44 -0500

    feat: add functionality to extractor_id

--- data/extractor_id.py
+++ data/extractor_id.py
@@ -5,9 +5,9 @@ import pandas as pd
 import shutil
 from sklearn.utils import shuffle
 
-def extraction(extraction_rate=0.1, stage_1_train_classes_csv="stage1_train_classes.csv", stage1_train_folder="stage1_train"):
+def extraction(extraction_rate=0.1, stage_1_train_classes_csv="stage1_train_classes.csv", stage1_train_folder="stage1_train", seed=1):
     if not os.path.exists(stage_1_train_classes_csv):
-        raise IOError("Make sure you have stage1_train_classes.csv")
+        raise IOError("Make sure you have " + stage_1_train_classes_csv)
     if not os.path.exists(stage1_train_folder):
         raise IOError("Make sure you have folder: stage1_train")
     with open(stage_1_train_classes_csv) as fr:
@@ -15,7 +15,7 @@ def extraction(extraction_rate=0.1, stage_1_train_classes_csv="stage1_train_clas
         res = [x for x in cr if os.path.exists('{0}/{1}'.format(stage1_train_folder, x['filename'].replace('.png','')))]
     assert(res != [])
     pd_data_origin = pd.DataFrame.from_records(res)
-    pd_data = shuffle(pd_data_origin, random_state = 1)
+    pd_data = shuffle(pd_data_origin, random_state = seed)
     pd_grouped = pd_data.groupby(["foreground", "background"])
     gps = pd_grouped.groups
     extracted = {}
@@ -27,4 +27,4 @@ def extraction(extraction_rate=0.1, stage_1_train_classes_csv="stage1_train_clas
     return result
 
 if __name__ == "__main__":
-    print(extraction(0.1, "stage1_train_classes.csv", "stage1_train"))
+    print(extraction(0.1, "stage1_train_classes.csv", "stage1_train", 1))
